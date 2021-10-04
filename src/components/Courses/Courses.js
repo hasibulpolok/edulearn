c66ea93335d10f9a032a3be9cc36c6d0a6390096
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-10-04 23:35:38 +0600

    course details added

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -13,12 +13,12 @@ const Courses = () => {
                 <h2 className="display-2 text-center fw-bold">Upcoming Courses</h2>
                 <hr className=" bg-danger mx-auto " />
 
-                <Row>
+                <Row className="m-5">
 
-                    <Course image={image1} title="Django Master Class" desc="" />
-                    <Course image={image2} title="Flutter Master Class" desc="" />
-                    <Course image={image3} title="GoLang Master Class" desc="" />
-                    <Course image={image4} title="Dart Master Class" desc="" />
+                    <Course image={image1} title="Django Master Class" desc="Django is very popular these days.You can do the backend development with Django" />
+                    <Course image={image2} title="Flutter Master Class" desc="Flutter is very popular and awesome for apps developemt. You can start for here" />
+                    <Course image={image3} title="GoLang Master Class" desc="Golang is very popular and awesome. Its developed by google. You can start from Edulearn" />
+                    <Course image={image4} title="Dart Master Class" desc="Dart is very very nice language with Dart you can build cross platform mobile apps" />
 
 
                 </Row>

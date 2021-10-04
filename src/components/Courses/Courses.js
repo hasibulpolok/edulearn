91c051ac75a9204624dbf158aa6fa3ab1ea47c9b
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-10-04 23:03:14 +0600

    mardown edited

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,13 +1,16 @@
+import react from 'react'
 import { useState, useEffect } from 'react';
 import Course from '../Course/Course';
 
-//  const [courses , setCourses] = useState([]);
-//  useEffect(()=>{
-//      fetch('./courses.JSON')
-//      .then(res => res.json())
-//      .then(data => setCourses(data))
-//  },[])
-//  console.log(courses);
+ const [courses , setCourses] = useState([]);
+ useEffect(()=>{
+     fetch('./courses.JSON')
+     .then(res => res.json())
+     .then(data => setCourses(data))
+ },[])
+
+ console.log(courses);
+
 const Courses = () => {
     return (
         <div>

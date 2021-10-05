8051affd16fee2db4bfe9a90b8c813a31f8f0398
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-10-05 18:19:02 +0600

    conatct bug fix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,69 +21,7 @@ const About = (props) => {
                 We take Conceptual Session everyday <br /> so that students can clear their all concept. You can choose any
                 course here and can start learning <br /> from today and you don't have to go any where just choose and start from your home ('-')
             </p>
-            <div>
-
-                <div className="upcoming-courses ms-4 mb-2">
-                    <h2 className="display-2 text-center fw-bold">{props.children}</h2>
-                    <hr className=" bg-danger mx-auto " />
-
-                    <Row>
-                        <Col md={3} xs={12}>
-                            <Card style={{ width: '18rem', marginBottom: "5px" }}>
-                                <Card.Img style={{ height: "100px" }} variant="top" src={djangoim} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col md={3} xs={12} >
-                            <Card style={{ width: '18rem', marginBottom: "5px" }}>
-                                <Card.Img variant="top" style={{ height: "100px" }} src={flutterim} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col md={3} xs={12}>
-                            <Card style={{ width: '18rem', marginBottom: "5px" }}>
-                                <Card.Img style={{ height: "100px" }} variant="top" src={dartim} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col md={3} xs={12}>
-                            <Card style={{ width: '18rem', marginBottom: "5px" }}>
-                                <Card.Img style={{ height: "100px" }} variant="top" src={golangim} />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-
-                    </Row>
-                </div>
-            </div>
+            
         </div>
 
     );
